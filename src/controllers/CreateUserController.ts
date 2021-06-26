185a1b040e02bb3441ac0fb5d4d3f229708b4350
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-06-25 21:14:56 -0300

    Create Middleware for treat errors

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -6,13 +6,9 @@ class CreateUserController {
     const { name, email, admin } = request.body;
 
     const createUserService = new CreateUserService();
-    try {
       const user = await createUserService.execute({ name, email, admin });
       console.log(user);
       return response.json(user);      
-    } catch (err) {
-        return response.json({"error": "User already exists"});
-    }
   }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+
 import { router } from './routes'; 
 
 import "./database";
@@ -8,5 +10,19 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+// Middleware de captura de erro - após a rota
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
