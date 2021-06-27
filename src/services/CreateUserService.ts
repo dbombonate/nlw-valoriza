46d17ab4f130344fe880d6d4401a761dc898e963
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-06-27 20:00:31 -0300

    Add password field on table users and refact code

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -4,12 +4,13 @@ import { UsersRepositories } from "../repositories/UserRepositories"
 interface IUserRequest {
   name: string;
   email: string;
-  admin?: boolean
+  admin?: boolean;
+  password: string;
 }
 
 class CreateUserService {
   
-  async execute({ name, email, admin } : IUserRequest) {
+  async execute({ name, email, admin, password } : IUserRequest) {
     const userRepository = getCustomRepository(UsersRepositories);
     
     if (!email) {
@@ -26,6 +27,7 @@ class CreateUserService {
       name,
       email,
       admin,
+      password,
     });
 
     await userRepository.save(user);
